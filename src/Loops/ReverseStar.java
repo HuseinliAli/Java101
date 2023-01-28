ad4c5131875dabb9dd0ab8573ad8609d71a1ca21
HuseinliAli <ali.guseynoov@gmail.com>
2023-01-28 13:31:55 +0400

    new commit

---
+++ src/Loops/ReverseStar.java
@@ -0,0 +1,19 @@
+package Loops;
+import java.util.Scanner;
+public class ReverseStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=n; i>0; i--){
+            for(int j = 0; j < n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i*2 -1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
