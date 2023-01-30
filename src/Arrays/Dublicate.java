bad6322a75404c0274d1787dc41f91d308056a67
HuseinliAli <ali.guseynoov@gmail.com>
2023-01-30 12:45:11 +0400

    new commit

--- src/Arrays/Dublicate.java
+++ src/Arrays/Dublicate.java
@@ -1,22 +1,19 @@
 package Arrays;
-
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Dublicate {
     public static void main(String[] args) {
-        int[] arr = {1,4,3,61,4,62,1,3,4,5,61,4,5,77,89,67,435};
-        int count =1;
-        int[] counts = new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(i != j  && arr[i] == arr[j]){
-                    count++;
-                    counts[i]= count;
-                }
+        int[] arr = {1, 2, 3, 1, 2, 3, 4};
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : arr) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
             }
-            System.out.println(arr[i] +" ededi "+ count +" denedir");
-            count = 1;
-            counts[i] = 1;
         }
+
+        System.out.println("Element counts: " + map);
     }
 }
